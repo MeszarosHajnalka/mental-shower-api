ab2924dea191372b543eb8e97c321030149fd0ff
MeszarosHajnalka <44954792+MeszarosHajnalka@users.noreply.github.com>
2021-04-17 00:12:25 +0200

    enable  dbconfig file

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "remotemysql.com",
+  USER: "hTwNV2Cncj",
+  PASSWORD: "kcxszI0U0T",
+  DB: "hTwNV2Cncj",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
