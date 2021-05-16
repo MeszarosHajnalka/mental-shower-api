8d3135d1d343e34d27e1cfe307164c393c2ab6eb
Isaksj <isaksj@gmail.com>
2021-05-16 16:09:02 +0200

    Update index.js

--- app/models/index.js
+++ app/models/index.js
@@ -17,7 +17,7 @@ const Sequelize = require("sequelize");
 const sequelize = new Sequelize('test', 'root', '', {
   host: "localhost",
   dialect: "mysql",
-  port: 3306
+  port: 3310
 });
 const db = {};
 
